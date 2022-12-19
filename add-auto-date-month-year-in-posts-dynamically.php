225a983b69ad84d207a37fcebcf0a7d083d62c15
Numan Rasheed <std.work@numanrki.com>
2022-12-19 16:38:24 +0500

    Bug Fixed

--- add-auto-date-month-year-in-posts-dynamically.php
+++ add-auto-date-month-year-in-posts-dynamically.php
@@ -100,12 +100,7 @@ function get_prev_month_aadmy() {
 }
 add_shortcode( 'p_month', 'get_prev_month_aadmy' );
 
-function get_aadmy_other_shortcodes(){
-  include 'other-functions.php';
-  add_shortcode('s_title', 'shortcode_get_domain_name_aadmy');
-  add_shortcode('f_copyrights', 'copyright_symbol_shortcode_aadmy');
-  add_shortcode('copy', 'copy_symbol_shortcode_aadmy');
-}
+
 // Also Work with WP Basic elements, Like Titles, Post Title, Expcerts
 add_filter( 'the_title', 'do_shortcode' );
 add_filter( 'single_post_title', 'do_shortcode' );
