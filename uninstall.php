0811a6e936ce44d03de8d3333f29a743393c62b2
numanrki <bhatti3742@gmail.com>
2023-03-28 20:05:32 +0500

    uninstall function fixed

--- uninstall.php
+++ uninstall.php
@@ -2,6 +2,3 @@
 if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
     exit;
   }
-
-  // Not like register_uninstall_hook(), you do NOT have to use a static function.
-  aadmyipd_fs()->add_action('after_uninstall', 'aadmyipd_fs_uninstall_cleanup');
